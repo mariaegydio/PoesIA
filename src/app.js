f6f271a3765098c54ef08e5d34d8929dd77e3db1
mariaegydio <egydiomj@gmail.com>
2025-05-24 12:13:32 -0300

    organizando pastas e arquivos

--- src/app.js
+++ src/app.js
@@ -1,21 +1,17 @@
 const express = require('express');
 require('dotenv').config();
-const connectDB = require('./config/db'); // Importa a conexão modularizada
+const connectDB = require('../config/db'); 
 
 const app = express();
 
-// Middleware
 app.use(express.json());
 
-// Rota simples de teste
 app.get('/', (req, res) => {
   res.send('API de Poemas funcionando');
 });
 
-// Conexão com o Banco de Dados
-connectDB(); // Chama a função de conexão
+connectDB(); 
 
-// Inicia o servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
